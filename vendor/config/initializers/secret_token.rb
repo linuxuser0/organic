39c37f77b4bd878327f6ddd4189ab3d89059d909
Jacob Waite <jacobsamuel1@comcast.net>
2013-07-17 14:26:44 -0700

    First Real Deal Code

---
+++ vendor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Organic::Application.config.secret_token = 'f31409fe8a29733ec87795bad8e1f92785a57729aa290d69fa52eee21fb46449265e71235749223ece5364e070c90cb6a46d821929f57b43fde2b3c4121f2b82'
